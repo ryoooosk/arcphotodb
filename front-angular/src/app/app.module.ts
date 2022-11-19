6e24f6bbe932403e9888aa242ace07d2269055ca
ryoooosk <pond.well.0205@gmail.com>
2022-11-19 15:52:20 +0900

    refactor: mypageコンポーネントとusereditコンポーネントをusersディレクトリ配下に

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -7,13 +7,13 @@ import { DashboardComponent } from './feature/dashboard/dashboard.component';
 import { DetailComponent } from './feature/detail/detail.component';
 import { SignupComponent } from './users/signup/signup.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MypageComponent } from './feature/mypage/mypage.component';
+import { MypageComponent } from './users/mypage/mypage.component';
 import { environment } from '../environments/environment';
 // Angular Module
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NewuserComponent } from './users/newuser/newuser.component';
-import { UsereditComponent } from './feature/useredit/useredit.component';
+import { UsereditComponent } from './users/useredit/useredit.component';
 // AngularFire
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth } from '@angular/fire/auth';
