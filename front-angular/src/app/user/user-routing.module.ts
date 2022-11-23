8a6d1ee2052f5ce55a3c91b5791bc7a98ae469bc
ryoooosk <pond.well.0205@gmail.com>
2022-11-23 19:01:02 +0900

    perf: mypage.htmlのrouterLinkをmypage/usereditni

--- front-angular/src/app/user/user-routing.module.ts
+++ front-angular/src/app/user/user-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MypageComponent } from './components/mypage/mypage.component';
 import { UsereditComponent } from './components/useredit/useredit.component';
-import { UserGuard } from '../core/guards/user.guard';
 
 const routes: Routes = [
   { path: '', component: MypageComponent },
