b55445841cc9f9c43d9850ce55fb719937b760d9
ryoooosk <pond.well.0205@gmail.com>
2023-01-01 21:32:26 +0900

    refactor: dashboard.componentのコードを見やすく改善

--- front-angular/src/app/feature/components/dashboard/dashboard.component.ts
+++ front-angular/src/app/feature/components/dashboard/dashboard.component.ts
@@ -28,44 +28,36 @@ export class DashboardComponent implements OnInit {
   openClose() {
     if(this.placeHide === true) {
       this.placeHide = false;
-    } else {
-      this.placeHide = true;
-    }
+    } else { this.placeHide = true; }
   }
 
   getPicture() {
-    // ↓初期化
-    this.tagService.tagPictures = null;
-    this.tagService.tagPicturesSrc = [];
-
-    const result = [];
-    for (let [key, value] of Object.entries(this.placeTags)) {
-      if(value.selected === true) {
-        result.push(value.id);
-      }
-    }
-    if(result.length) {
-      console.log(result);
-    }
-
+    const result = this.loop(this.placeTags);
     this.tagService.getPictures(result)
-      .pipe(
-        tap((response) => {
-          if(response) {
-            this.tagService.tagPictures = response;
-            console.log(this.tagService.tagPictures);
-            for(let i = 0; i < this.tagService.tagPictures.length; i+=1) {
-              this.tagService.tagPicturesSrc.push(this.tagService.tagPictures[i].path);
-            }
+      .subscribe((response) => {
+        // 初期化
+        this.tagService.tagPictures = null;
+        this.tagService.tagPicturesSrc = [];
+
+        if(response) {
+          this.tagService.tagPictures = response;
+          console.log(this.tagService.tagPictures);
+          for(let i = 0; i < this.tagService.tagPictures.length; i+=1) {
+            this.tagService.tagPicturesSrc.push(this.tagService.tagPictures[i].path);
           }
-        }),
-      )
-      .subscribe({
-        error: (error) => console.log(error),
-        complete: () => {
-          console.log('Get tagPictures!');
         }
       });
+  }
+
+  loop(tag: object): any[] {
+    const result: any[] = [];
+    for (let [key, value] of Object.entries(tag)) {
+      if(value.selected === true) { result.push(value.id); }
+    }
+    if(result.length) { console.log(result); }
+    else { console.log("No Tags"); }
+
+    return result;
   }
 
 }
